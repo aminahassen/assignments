ee9d83c46f63d8249e2afbf83564ee55a9a20825
aminahassen <aminahassen10@gmail.com>
2023-05-25 16:49:20 +0300

    done

--- DOM/js/card.js
+++ DOM/js/card.js
@@ -1,4 +1,3 @@
-// dhamaan elements ka 
 let body = document.querySelector('body');
 let container = document.createElement('div')
 let flex = document.createElement('div');
@@ -6,7 +5,7 @@ let h1 = document.createElement('h1');
 let img = document.createElement('img');
 let date = document.createElement('h4');
 let p =  document.createElement('p');
-let like = document.createElement(`img`);
+let icon = document.createElement(`img`);
 
 body.appendChild(container)
 body.appendChild(img)
@@ -15,7 +14,7 @@ flex.appendChild(h1)
 flex.appendChild(img)
 container.appendChild(date)
 container.appendChild(p)
-flex.appendChild(like)
+flex.appendChild(icon)
 
 let h1Style = `
 transform: translateX(70px)translateY(6px);
@@ -29,18 +28,20 @@ border-radius: 50px;
 height: 50px;
 `
 
-let likeStyle = `
-transform: translateY(260%) translateX(-45px)
+let iconStyle = `
+transform: translateY(100px) translateX(-40px);
+width: 200px;
+height: 30px;
 `
 
 let containerStyle = `
-    background-color: #D5D3CF;
-    width: 500px;
-    height: 250px;
+background-color: grey;
+width: 500px;
+height: 250px;
 `
 
 let dateStyle = `
-transform: translateY(-90px)translateX(100px);
+transform: translateY(-90px)translateX(90px);
 color: black;
 font-size:14px;
 `
@@ -51,8 +52,7 @@ display: flex;
 `
 
 let pStyle = `
-transform: translateY(-100px);
-margin-left: 9px;
+transform: translateY(-100px) translateX(7px);s
 `
 
 
@@ -66,5 +66,5 @@ date.textContent = "Public - 09JUN,2021"
 date.style.cssText = dateStyle
 img.src = `../img/wondarwoman.jpg`
 img.style.cssText = imgStyle
-like.style.cssText = likeStyle
-like.src = `../img/like.png`
+icon.style.cssText = iconStyle
+icon.src = `../img/likeicon.jpg`
